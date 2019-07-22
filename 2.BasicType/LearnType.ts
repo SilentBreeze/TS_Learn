c5471ce68a2a9569f48649e47807862c24e867ea
SilentBreeze <sunshine_pro@163.com>
2019-07-22 22:11:25 +0800

    Learn Basic Type

---
+++ 2.BasicType/LearnType.ts
@@ -0,0 +1,28 @@
+let binaryLiteral: number = 0b100;
+let octalLiteral: number = 0o744;
+let decLiteral: number = 6;
+let hexLiteral: number = 0xf00d;
+
+let siteName: string = "Runoob";
+let years: number = 5;
+let words: string = `Hello, ${siteName} has been published for ${years} years.`;
+
+let flag: boolean = true;
+
+let arr1: number[] = [1, 2];
+let arr2: Array<number> = [1, 2];
+
+let map: [string, number];
+map = ["Runoob", 1];
+
+enum Color { Red, Green, Blue };
+let c: Color = Color.Blue;
+
+function helloWorlsd(): void {
+    alert("Hello World!");
+}
+
+let x: any = 1;
+let anyList: any[] = [1, false, "test"];
+
+console.log();
