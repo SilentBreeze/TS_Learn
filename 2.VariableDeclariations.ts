3ff8bfee7647e49edd3e27a872f08d22ef431bee
SilentBreeze <sunshine_pro@163.com>
2019-08-30 09:21:26 +0800

    Update Chapter 2

--- 2.VariableDeclariations.ts
+++ 2.VariableDeclariations.ts
@@ -25,4 +25,36 @@ function f2() {
     }
 }
 var g = f2();
-g();
+g(); // returns '11'
+// In this above example, 'g' captured the variable 'a' declared in 'f2'.
+// At any point that 'g' gets called, the value of 'a' will be tied to the value of 'a' in 'f2'.
+// Even if 'g' is called once 'f2' is done running, it will be able to access and modify 'a'.
+function f3() {
+    var a = 1;
+    a = 2;
+    var b = g();
+    a = 3;
+    return b;
+
+    function g() {
+        return a;
+    }
+}
+f3(); // returns '2'
+
+// Scoping rules
+// 'var' declarations have some odd scoping rules for those used to other languages.
+// Take the following example:
+function f4(shouldInitialize: boolean) {
+    if (shouldInitialize) {
+        var x = 10;
+    }
+    return x;
+}
+f4(true); // return '10'
+f4(false); // return 'undefined'
+// Some readers might do a double-take at this example. 
+// The variable 'x' was declared within the 'if' block, and yet we were able to access it from outside that block.
+// That's because 'var' declarations are accessible anywhere within their containing function, module, namespace, or global scope - all which we'll go over later on - regardless of the containing block.
+// Some people call this 'var'-scoping or function-scoping.
+// Parameters are also function scoped. 
